b383d4db8b8e0900c0e9657eacfee2afb03c367f
Vincenzo <lisitano95@gmail.com>
2025-06-29 13:15:51 +0200

    risoluzione responsive

--- main.js
+++ main.js
@@ -123,20 +123,4 @@ stars.forEach((star, index)=>{
         star.appendChild(icon)
     }
 });
-
-// Swiper
-  var swiper = new Swiper(".mySwiper", {
-        slidesPerView: 1,
-        spaceBetween: 30,
-        keyboard: {
-          enabled: true,
-        },
-        pagination: {
-          el: ".swiper-pagination",
-          clickable: true,
-        },
-        navigation: {
-          nextEl: ".swiper-button-next",
-          prevEl: ".swiper-button-prev",
-        },
 });
